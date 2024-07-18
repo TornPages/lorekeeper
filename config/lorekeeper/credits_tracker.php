46b42fab348d758fe04dc080eeb7d6ebecad4b7f
Cassius Redwood <162834015+TornPages@users.noreply.github.com>
2024-07-17 22:00:43 -0300

    Create credits_tracker.php

---
+++ config/lorekeeper/credits_tracker.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    // FORMAT EXAMPLE.
+    //
+    // 'TornPages' => [
+    //     'key' => 'TornPages',
+    //     'creator' => 'T0rnPages',
+    //     'credits' => json_encode([
+    //         'Item: Sprout Key' => 'https://www.jellocats.club/world/items/103',
+    //     ]),
+    // ],
+
+];
