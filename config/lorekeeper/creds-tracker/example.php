fa9acd2bde348d06a0e5a88ce8cca4dc810bfe31
Cassius Redwood <162834015+TornPages@users.noreply.github.com>
2024-07-17 22:53:53 -0300

    Update example.php

--- config/lorekeeper/creds-tracker/example.php
+++ config/lorekeeper/creds-tracker/example.php
@@ -1,4 +1,4 @@
-<!-- This is just an example of an artist credit. Create a new file in config/lorekeeper/creds-tracker for it to register properly. Delete this file once you have created your own credits! 
+<!-- This is just an example of an artist credit. Create a new file in config/lorekeeper/creds-tracker for it to register properly. Delete this file (or edit it) once you have created your own credits! 
 ~ Creator = DeviantArt Username
 
 Put each new credit on a new line
