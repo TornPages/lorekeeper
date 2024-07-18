c3a82ade4d5a7852edebc64389da909820d61ba5
Cassius Redwood <162834015+TornPages@users.noreply.github.com>
2024-07-17 22:29:07 -0300

    Create artist_credits_tracker.php

---
+++ config/lorekeeper/ext-tracker/artist_credits_tracker.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'wiki_key' => 'Artist_Credits_Tracker',
+    'creators' => json_encode([
+        'TornPages' => 'https://github.com/TornPages/',
+    ]),
+    'version' => '1.0.0',
+];
